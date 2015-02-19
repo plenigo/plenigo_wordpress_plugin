556e2374f850d23fa3e352e6d861f0d269cdc4f7
Sebastian Dieguez <s.dieguez@plenigo.com>
2015-02-19 12:17:40 -0300

    = 1.1.21 - Fixes for URL redirect = - Fixed a problem with URL parameters being stored as a return URL

--- plenigo_plugin/PlenigoLoginManager.php
+++ plenigo_plugin/PlenigoLoginManager.php
@@ -27,6 +27,11 @@ class PlenigoLoginManager
     const PLENIGO_META_NAME = 'plenigo_uid';
 
     /**
+     * Holds the list of forbidden URL query parameters
+     */
+    const FORBIDDEN_PARAMS = "plppsuccess,plppfailure,token,PayerID,plsofortsuccess,plsofortfailure,plpfsuccess,plpffailure";
+
+    /**
      * Holds the values to be used in the fields callbacks
      */
     private $options = null;
@@ -331,6 +336,7 @@ class PlenigoLoginManager
     public function store_url()
     {
         $current_url = $this->full_url($_SERVER, true);
+        $current_url = $this->sanitize_url($current_url);
         $_SESSION['plenigo_throwback_url'] = $current_url;
     }
 
@@ -363,6 +369,54 @@ class PlenigoLoginManager
     private function full_url($s, $use_fwd_host = false)
     {
         return $this->url_origin($s, $use_fwd_host) . $s['REQUEST_URI'];
+    }
+
+    /**
+     * Strip certain parameters from the URL for the sake of cohesive experience for the users.
+     * @param string $current_url the URL to strip from the query parameters
+     */
+    private function sanitize_url($current_url)
+    {
+        $res = $current_url; // a safe default
+        $arrParsedURL = parse_url($current_url);
+        $arrParsedQuery = array();
+        if ($arrParsedURL !== FALSE && !is_null($arrParsedURL)) {
+            parse_str($arrParsedURL['query'], $arrParsedQuery);
+            plenigo_log_message("QueryString:" . var_export($arrParsedQuery, true), E_USER_NOTICE);
+            $arrFilteredQuery = array_diff_key($arrParsedQuery, array_flip(explode(",", self::FORBIDDEN_PARAMS)));
+            plenigo_log_message("QueryString Filtered:" . var_export($arrFilteredQuery, true), E_USER_NOTICE);
+            $arrParsedURL['query'] = http_build_query($arrFilteredQuery);
+            plenigo_log_message("URL Filtered:" . var_export($arrParsedURL, true), E_USER_NOTICE);
+            $res = $this->unparse_url($arrParsedURL);
+            plenigo_log_message("URL Filtered:" . $res, E_USER_NOTICE);
+        }
+        return $res;
+    }
+
+    /**
+     * Manual replacement of http_build_url() to avoid PECL library requirement
+     * 
+     * @param type $parsed_url
+     * @return type
+     */
+    private function unparse_url($parsed_url)
+    {
+        //unset blank values
+        foreach ($parsed_url as $key => $value) {
+            if (isset($parsed_url[$key]) && trim($parsed_url[$key]) === '') {
+                unset($parsed_url[$key]);
+            }
+        }
+        $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+        $host = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+        $port = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+        $user = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+        $pass = isset($parsed_url['pass']) ? ':' . $parsed_url['pass'] : '';
+        $pass = ($user || $pass) ? $pass . "@" : '';
+        $path = isset($parsed_url['path']) ? $parsed_url['path'] : '';
+        $query = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
+        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+        return $scheme . $user . $pass . $host . $port . $path . $query . $fragment . '';
     }
 
 }
