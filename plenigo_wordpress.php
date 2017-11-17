253aa09b1cbb06ecafa7ded0e7f77b48a321520b
Maximilian Schweitzer <maximilian.schweitzer@plenigo.com>
2017-11-17 13:37:31 +0100

    + updated to new WP version

--- plenigo_wordpress.php
+++ plenigo_wordpress.php
@@ -4,16 +4,16 @@
   Plugin Name: Plenigo
   Plugin URI: http://wordpress.org/plugins/plenigo/
   Description: So far, the technical implementation of paid content has been time-consuming and costly for publishing houses and media companies. plenigo puts an end to this.
-  Version: 1.7.2
+  Version: 1.7.3
   Author: Plenigo
   Author URI: https://www.plenigo.com
   Text Domain: plenigo
   License: GPLv2
   WC requires at least: 4.0
-  WC tested up to: 4.8.3
+  WC tested up to: 4.9.0
  */
 /*
-  Copyright (C) 2014 plenigo
+  Copyright (C) 2017 plenigo
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
@@ -29,7 +29,7 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-define('PLENIGO_VERSION', '1.7.2');
+define('PLENIGO_VERSION', '1.7.3');
 
 // Plenigo JavaScript SDK / Services
 define('PLENIGO_SVC_URL', "https://api.plenigo.com");
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: Sebastian Dieguez <s.dieguez@plenigo.com>
 Tags: paywall, e-commerce, Ecommerce, paid content software, subscriptions, newspaper, media, pay-per-read, pay, plugin, donate, money, transaction, bank, visa, mastercard, credit, debit, card, widget, give, pay what you want, plenigo, payment
 Requires at least: 4.0.0
-Tested up to: 4.8.3
-Stable tag: 1.7.2
+Tested up to: 4.9.0
+Stable tag: 1.7.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -298,6 +298,9 @@ separator, native Wordpress tag.
 /assets/screenshot-2.png
 
 == Changelog ==
+= 1.7.3 - Updated dependencies =
+- Upgraded plenigo PHP SDK version
+
 = 1.7.2 - Improved stability =
 - Upgraded plenigo PHP SDK version
 
